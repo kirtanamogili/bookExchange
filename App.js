dfc195965c8ddafe22a67d37aee25f505b50eb95
Nevin Gilbert <nevin.gil@live.com>
2021-06-30 01:03:49 -0600

    revamp project structure

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { render } from 'react-dom';
 import Realm from 'realm';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Logon from './src/components/Logon'
 let realm;
 
 export default class App extends React.Component {
@@ -54,38 +55,12 @@ export default class App extends React.Component {
       }
     } 
   };
-  
+
   render(){
-    return (
-      <View style={styles.container}>
-        <Text style={styles.logo}>bookExchange</Text>
-        <View style={styles.inputView} >
-          <TextInput
-            style={styles.inputText}
-            placeholder="Email..."
-            placeholderTextColor = "#003f5c"
-            onChangeText={text => this.setState({email:text})}/>
-        </View>
-        <View style={styles.inputView} >
-          <TextInput
-            secureTextEntry
-            style={styles.inputText}
-            placeholder={"Pasword..."}
-            placeholderTextColor="#003f5c"
-            onChangeText={text=> this.setState({password:text})} />
-        </View>
-        <TouchableOpacity>
-          <Text style={styles.forgot}>Forgot Password?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.loginText}>Signup</Text>
-        </TouchableOpacity>
-      </View>
-    );
+    <Logon/>
+    
   }
+
 }
 
 const UserSchema = {
@@ -105,47 +80,3 @@ const realm = await Realm.open({
   schema: [UserSchema],
 });
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#607B7D',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo : {
-    fontWeight:"bold",
-    fontSize:50,
-    color: "#FB9B5A",
-    marginBottom:40,
-  },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#3A606E",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText:{
-    height:50,
-    color:"white"
-  },
-  forgot:{
-    color:"white",
-    fontSize:11
-  },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#98473E",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-  loginText:{
-    color:"white"
-  }
-});
---
+++ src/components/Logon.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, TextInput, TouchableOpacity, View } from 'react-native';
+import './Logon.css'
+
+export default class Logon extends React.Component {
+    render() {
+        <View class='container'>
+        <Text class='logo'>bookExchange</Text>
+        <View class='inputView' >
+            <TextInput
+            class='inputText'
+            placeholder="Email..."
+            placeholderTextColor = "#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <View class='inputView' >
+            <TextInput
+            secureTextEntry
+            class='inputText'
+            placeholder={"Pasword..."}
+            placeholderTextColor="#003f5c"
+            onChangeText={text=> this.setState({password:text})} />
+        </View>
+        <TouchableOpacity>
+            <Text class='forgot'>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity class='loginBtn'>
+            <Text class='loginText'>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <Text class='loginText'>Signup</Text>
+        </TouchableOpacity>
+        </View>
+    }
+}
