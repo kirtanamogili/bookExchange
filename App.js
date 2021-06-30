266dd5bc298b6d485c201d229705ca8109794fa6
Nevin Gilbert <nevin.gil@live.com>
2021-06-29 23:17:01 -0600

    added unstaged files

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state={
+    email:"",
+    password:""
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>bookExchange</Text>
+        <View style={styles.inputView} >
+          <TextInput
+            style={styles.inputText}
+            placeholder="Email..."
+            placeholderTextColor = "#003f5c"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput
+            secureTextEntry
+            style={styles.inputText}
+            placeholder={"Pasword..."}
+            placeholderTextColor="#003f5c"
+            onChangeText={text=> this.setState({password:text})} />
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.forgot}>Forgot Password?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.loginText}>Signup</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#607B7D',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  logo : {
+    fontWeight:"bold",
+    fontSize:50,
+    color: "#FB9B5A",
+    marginBottom:40,
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#3A606E",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"white"
+  },
+  forgot:{
+    color:"white",
+    fontSize:11
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#98473E",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  }
 });
