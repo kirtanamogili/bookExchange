b5aa460a77f84a938e7a6ae9a06ee0462f482734
Nevin Gilbert <nevin.gil@live.com>
2021-07-13 00:11:13 -0600

    Revamped file structure now renders, however styling is lost

--- App.js
+++ App.js
@@ -1,82 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom';
-import Realm from 'realm';
+import { View } from 'react-native';
 import Logon from './src/components/Logon'
-let realm;
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={
-      email:"",
-      password:""
-    };
-    realm = new Realm({
-      path: 'UserDatabase.realm',
-      schema: UserSchema,
-    });
-  }
-
-  register_user = () => {
-    var that = this;
-    const { email } = this.state;
-    const { password } = this.state;
-    if (email) {
-      if (password) {
-          realm.write(() => {
-            var ID =
-              realm.objects('User').sorted('first_name', true).length > 0
-                ? realm.objects('User').sorted('user_id', true)[0]
-                    .user_id + 1
-                : 1;
-            realm.create('user_details', {
-              user_id: ID,
-              user_name: that.state.user_name,
-              user_contact: that.state.user_contact,
-              user_address: that.state.user_address,
-            });
-            Alert.alert(
-              'Success',
-              'You are registered successfully',
-              [
-                {
-                  text: 'Ok',
-                  onPress: () => that.props.navigation.navigate('HomeScreen'),
-                },
-              ],
-              { cancelable: false }
-            );
-          });
-        } else{
-          alert('Please fill Password');  
-        }
-      } else {
-        alert('Please fill Email');
-      }
-    } 
-  };
-
+class App extends React.Component {
   render(){
-    <Logon/>
-    
+    return (
+      <Logon/>
+    );
   }
-
 }
 
-const UserSchema = {
-  name: 'User',
-  properties: {
-    _id: 'objectId?',
-    email: 'string?',
-    first_name: 'string?',
-    last_name: 'string?',
-    location: 'string?',
-  },
-  primaryKey: '_id',
-};
-
-const realm = await Realm.open({
-  path: "userData",
-  schema: [UserSchema],
-});
+export default App;
 
--- src/components/Logon.js
+++ src/components/Logon.js
@@ -3,33 +3,45 @@ import { Text, TextInput, TouchableOpacity, View } from 'react-native';
 import './Logon.css'
 
 export default class Logon extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            email:"",
+            password:""
+        }
+    }
+    
     render() {
-        <View class='container'>
-        <Text class='logo'>bookExchange</Text>
-        <View class='inputView' >
-            <TextInput
-            class='inputText'
-            placeholder="Email..."
-            placeholderTextColor = "#003f5c"
-            onChangeText={text => this.setState({email:text})}/>
-        </View>
-        <View class='inputView' >
-            <TextInput
-            secureTextEntry
-            class='inputText'
-            placeholder={"Pasword..."}
-            placeholderTextColor="#003f5c"
-            onChangeText={text=> this.setState({password:text})} />
-        </View>
-        <TouchableOpacity>
-            <Text class='forgot'>Forgot Password?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity class='loginBtn'>
-            <Text class='loginText'>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-            <Text class='loginText'>Signup</Text>
-        </TouchableOpacity>
-        </View>
+        return(
+            <div>
+                <View class='container'>
+                <Text class='logo'>bookExchange</Text>
+                <View class='inputView' >
+                    <TextInput
+                    class='inputText'
+                    placeholder="Email..."
+                    placeholderTextColor = "#003f5c"
+                    onChangeText={text => this.setState({email:text})}/>
+                </View>
+                <View class='inputView' >
+                    <TextInput
+                    secureTextEntry
+                    class='inputText'
+                    placeholder={"Pasword..."}
+                    placeholderTextColor="#003f5c"
+                    onChangeText={text=> this.setState({password:text})} />
+                </View>
+                <TouchableOpacity>
+                    <Text class='forgot'>Forgot Password?</Text>
+                </TouchableOpacity>
+                <TouchableOpacity class='loginBtn'>
+                    <Text class='loginText'>LOGIN</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text class='loginText'>Signup</Text>
+                </TouchableOpacity>
+                </View>
+            </div>
+        );
     }
 }
